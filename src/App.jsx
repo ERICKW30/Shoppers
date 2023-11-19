023a3cae70a15608aab388272112c918ac6799e9
neville777 <neville.james@student.moringaschool.com>
2023-11-19 07:09:25 +0300

    Components and Pages

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "./Component/Navbar/Navbar";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
