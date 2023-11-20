c668173227b892d3a6c43c8ceb9ae8f9c57982d5
neville777 <neville.james@student.moringaschool.com>
2023-11-20 08:17:56 +0300

    styling the ShopCategory component

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,12 +1,37 @@
 import { useContext } from "react";
 import { ShopContext } from "../Context/ShopContext";
+import Item from "../Components/Item/Item";
 import "./CSS/ShopCategory.css";
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+
 const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
 
   return (
     <div className="shop-category">
-      <img src={props.banner} alt="" />
+      <img className="shopcategory-banner" src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product
+          .filter((item) => props.category === item.category)
+          .map((item) => (
+            <Item
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          ))}
+      </div>
     </div>
   );
 };
