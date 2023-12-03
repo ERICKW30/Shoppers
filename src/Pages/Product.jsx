b6e76f5849f406969e8f2f39ee40c21cedd058b9
neville777 <neville.james@student.moringaschool.com>
2023-12-03 07:17:44 +0300

    The product.jsx

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,4 +1,19 @@
-const Product = () => {
-  return <div>Product</div>;
+import ".Item.css";
+import { link } from "react-router-dom";
+
+const Product = (props) => {
+  return (
+    <div className="item">
+      <link to={"/product/${props.id"}>
+        <img src={props.image} alt="" />
+      </link>
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">${props.new_price}</div>
+        <div className="item-price-old">${props.old_price}</div>
+      </div>
+    </div>
+  );
 };
+
 export default Product;
