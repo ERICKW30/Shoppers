262697745877d99a28c1fdbc99f12c9c69bf60f4
neville777 <neville.james@student.moringaschool.com>
2023-12-30 17:29:53 +0300

    Styling the DescriptionBox

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import "./DescriptionBox.css";
 
 const DescriptionBox = ({ product }) => {
   return (
     <div className="descriptionbox">
-      <div className="description-navigator">
-        <div className="description-nav-box">Description</div>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
         <div className="descriptionbox-nav-box-fade">Reviews (107)</div>
       </div>
       <div className="descriptionbox-description">{product.description}</div>
