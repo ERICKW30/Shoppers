ef55cb428fb374b6026cee51d85a62cc09885115
neville777 <neville.james@student.moringaschool.com>
2024-01-02 17:10:51 +0300

    Added Breakpoints to the App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ShopContextProvider from "./Context/ShopContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <ShopContextProvider>
-    <App />
+    <ThemeProvider
+      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+      minBreakpoint="xxs"
+    >
+      <App />
+    </ThemeProvider>
   </ShopContextProvider>
 );
