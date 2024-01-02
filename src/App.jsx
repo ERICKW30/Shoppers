9500c78791f12a62985911f33feb6115fd217c91
neville777 <neville.james@student.moringaschool.com>
2024-01-02 11:26:57 +0300

    Styling the app.jsx with bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import Navbar from "./Components/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
@@ -19,24 +20,26 @@ function App() {
       <BrowserRouter>
         <Navbar />
 
-        <Routes>
-          <Route
-            path="/mens"
-            element={<ShopCategory banner={men_banner} category="men" />}
-          />
-          <Route
-            path="/womens"
-            element={<ShopCategory banner={women_banner} category="women" />}
-          />
-          <Route
-            path="/kids"
-            element={<ShopCategory banner={kid_banner} category="kid" />}
-          />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/loginSignup" element={<LoginSignup />} />
-          <Route exact path="/" element={<Shop />} />
-        </Routes>
+        <Container>
+          <Routes>
+            <Route
+              path="/mens"
+              element={<ShopCategory banner={men_banner} category="men" />}
+            />
+            <Route
+              path="/womens"
+              element={<ShopCategory banner={women_banner} category="women" />}
+            />
+            <Route
+              path="/kids"
+              element={<ShopCategory banner={kid_banner} category="kid" />}
+            />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/loginSignup" element={<LoginSignup />} />
+            <Route exact path="/" element={<Shop />} />
+          </Routes>
+        </Container>
 
         <Footer />
       </BrowserRouter>
