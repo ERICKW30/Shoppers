c14da2e41923b53cb6273e810affa299d8748fa3
neville777 <neville.james@student.moringaschool.com>
2024-01-05 09:30:37 +0300

    Styling the Cartsitem.jsx

--- src/Components/CartsItems/CartsItems.jsx
+++ src/Components/CartsItems/CartsItems.jsx
@@ -1,73 +1,97 @@
-import "./CartsItems.css";
+import { Table } from "react-bootstrap";
 import { ShopContext } from "../../Context/ShopContext";
 import { useContext } from "react";
 import remove_icon from "../Assets/cart_cross_icon.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const CartsItems = () => {
   const { getTotalCartAmount, all_product, cartItems, removeFromCart } =
     useContext(ShopContext);
 
   return (
-    <div className="cartitems">
-      <div className="cartitems-format-main">
-        <p>Products</p>
-        <p>Title</p>
-        <p>Price</p>
-        <p>Quantity</p>
-        <p>Total</p>
-        <p>Remove</p>
-      </div>
-      <hr />
-      {all_product.map((e) => {
-        if (cartItems[e.id] > 0) {
-          return (
-            <div key={e.id}>
-              <div className="cartitems-format cartitems-format-main">
-                <img src={e.image} alt="" className="carticon-product-icon" />
-                <p>{e.name}</p>
-                <p>Ksh.{e.new_price}</p>
-                <button className="cartitems-quantity">
-                  {cartItems[e.id]}
-                </button>
-                <p>Ksh.{e.new_price * cartItems[e.id]}</p>
-                <img
-                  className="cartitems-remove-icon"
-                  src={remove_icon}
-                  onClick={() => removeFromCart(e.id)}
-                />
-              </div>
-              <hr />
-            </div>
-          );
-        }
-        return null;
-      })}
-      <div className="cartitems-down">
-        <div className="cartitems-total">
-          <h1>cart Totals</h1>
-          <div>
-            <div className="cartitems-total-item">
-              <p>Subtotal</p>
-              <p>Ksh{getTotalCartAmount()}</p>
-            </div>
-            <hr />
-            <div className="cartitems-total-item">
-              <p>Shipping Fee</p>
-              <p>Free</p>
-            </div>
-            <hr />
-            <div className="cartitems-total-item">
-              <h3>Total</h3>
-              <h3>Ksh{getTotalCartAmount()}</h3>
-            </div>
-          </div>
-          <button>PROCEED TO CHECKOUT</button>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-lg-8" style={{ width: "100%" }}>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>Products</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              {all_product.map((e) => {
+                if (cartItems[e.id] > 0) {
+                  return (
+                    <tr key={e.id}>
+                      <td>
+                        <img
+                          src={e.image}
+                          alt=""
+                          className="carticon-product-icon"
+                        />
+                      </td>
+                      <td>{e.name}</td>
+                      <td>Ksh.{e.new_price}</td>
+                      <td>
+                        <button className="cartitems-quantity btn btn-light">
+                          {cartItems[e.id]}
+                        </button>
+                      </td>
+                      <td>Ksh.{e.new_price * cartItems[e.id]}</td>
+                      <td>
+                        <img
+                          className="cartitems-remove-icon"
+                          src={remove_icon}
+                          alt="Remove"
+                          onClick={() => removeFromCart(e.id)}
+                        />
+                      </td>
+                    </tr>
+                  );
+                }
+                return null;
+              })}
+            </tbody>
+          </Table>
         </div>
-        <div className="cartitems-promocode">
-          <p>If you have a promo code, Enter it here</p>
-          <div className="cartitems-promobox">
-            <input type="text" placeholder="promo code" />
-            <button>submit</button>
+      </div>
+      <div className="col-lg-4">
+        <div className="cartitems-total mt-3">
+          <h1>Cart Totals</h1>
+          <Table bordered responsive>
+            <tbody>
+              <tr>
+                <td>Subtotal</td>
+                <td>Ksh{getTotalCartAmount()}</td>
+              </tr>
+              <tr>
+                <td>Shipping Fee</td>
+                <td>Free</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>Ksh{getTotalCartAmount()}</td>
+              </tr>
+            </tbody>
+          </Table>
+          <button className="cartitems-total-button btn btn-danger mt-4 h5">
+            PROCEED TO CHECKOUT
+          </button>
+        </div>
+        <div className="cartitems-promocode mt-4">
+          <h4>If you have a promo code, Enter it here.</h4>
+          <div className="cartitems-promobox d-flex">
+            <input
+              type="text"
+              className="form-control "
+              placeholder="Promo code"
+            />
+            <button className="btn btn-dark ml-2">Submit</button>
           </div>
         </div>
       </div>
