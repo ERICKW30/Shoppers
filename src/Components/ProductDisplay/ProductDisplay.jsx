c023de48b0c3a8c893019fdb054043ae2d187229
neville777 <neville.james@student.moringaschool.com>
2024-01-05 05:35:16 +0300

    Syling the productDisplay right-side

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,70 +1,95 @@
-import "./ProductDisplay.css";
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
 import { useContext } from "react";
 import { ShopContext } from "../../Context/ShopContext";
-import { Button } from "react-bootstrap";
+import { Button, Col, Image, Row } from "react-bootstrap";
 
 const ProductDisplay = ({ product }) => {
   const { addToCart } = useContext(ShopContext);
 
   return (
-    <div className="productdisplay">
-      <div className="productdisplay-left">
-        <div className="productdisplay-img-list">
-          <img src={product.image} alt="" />
-          <img src={product.image} alt="" />
-          <img src={product.image} alt="" />
-          <img src={product.image} alt="" />
+    <Row>
+      <Col sm={2} md={6} lg={6} xl={6}>
+        <div className="d-flex flex-column gap-1 overflow-auto mt-5">
+          <Image src={product.image} alt="" className="mb-3" />
+          <Image src={product.image} alt="" className="mb-3" />
+          <Image src={product.image} alt="" className="mb-3" />
+          <Image src={product.image} alt="" className="mb-2" />
         </div>
-        <div className="productdisplay-img">
-          <img className="productdisplay-main-img" src={product.image} />
-        </div>
-      </div>
-      <div className="productdisplay-right">
-        <h1>{product.name}</h1>
-        <div className="productdisplay-right-star">
-          <img src={star_icon} alt="" />
-          <img src={star_icon} alt="" />
-          <img src={star_icon} alt="" />
-          <img src={star_icon} alt="" />
-          <img src={star_dull_icon} alt="" />
-          <p>(122)</p>
-        </div>
-        <div className="productdisplay-right-prices">
-          <div className="productdisplay-right-price-old">
-            ${product.old_price}
+      </Col>
+
+      <Col sm={1} md={6}>
+        <div>
+          <h1>{product.name}</h1>
+          <div className="d-flex align-items-center gap-2 mt-5">
+            <Image src={star_icon} alt="" />
+            <Image src={star_icon} alt="" />
+            <Image src={star_icon} alt="" />
+            <Image src={star_icon} alt="" />
+            <Image src={star_dull_icon} alt="" />
+            <p>177</p>
           </div>
-          <div className="productdisplay-right-price-new">
-            ${product.new_price}
+          <div
+            className="d-flex mt-4 col-sm-12"
+            style={{ fontSize: ".8cm", gap: "45px" }}
+          >
+            <div className="text-decoration-line-through text-danger">
+              Ksh.{product.old_price}
+            </div>
+            <div className="text-success">Ksh.{product.new_price}</div>
+          </div>
+          <div className="mt-5">
+            <h1>Select Size</h1>
+            <div className="d-flex gap-4 mt-5 " style={{ margin: "0px 14px" }}>
+              <Button
+                variant="success"
+                className="productdisplay-right-size-option"
+              >
+                S{" "}
+              </Button>
+              <Button
+                variant="success"
+                className="productdisplay-right-size-option"
+              >
+                M
+              </Button>
+              <Button
+                variant="success"
+                className="productdisplay-right-size-option"
+              >
+                L
+              </Button>
+              <Button
+                variant="success"
+                className="productdisplay-right-size-option"
+              >
+                XL
+              </Button>
+              <Button
+                variant="success"
+                className="productdisplay-right-size-option"
+              >
+                XXL
+              </Button>
+            </div>
+          </div>
+          <Button
+            variant="success"
+            style={{ width: "100%" }}
+            onClick={() => {
+              addToCart(product.id);
+            }}
+            className="mt-5"
+          >
+            Add to cart
+          </Button>
+          <div className="mt-5 d-flex flex-column gap-2">
+            <h5 className="mt-3">Category : Woman, T-shirt, Crop top</h5>
+            <h5>Tags : Modern, Latest</h5>
           </div>
         </div>
-        <div className="productdisplay-right-size">
-          <h1>Select Size</h1>
-          <div className="productdisplay-right-size">
-            <div>S</div>
-            <div>M</div>
-            <div>L</div>
-            <div>XL</div>
-            <div>XXL</div>
-          </div>
-        </div>
-        <Button
-          variant="success"
-          onClick={() => {
-            addToCart(product.id);
-          }}
-        >
-          Add to cart
-        </Button>
-        <p className="productdisplay-right-category">
-          <span>Category : </span>Woman , T-shirt, Crop top
-        </p>
-        <p className="productdisplay-right-category">
-          <span>Tags : </span>Morden , Latest
-        </p>
-      </div>
-    </div>
+      </Col>
+    </Row>
   );
 };
 
